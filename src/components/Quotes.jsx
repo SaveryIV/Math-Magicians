a40759893cd77d090fc38f8fad8bfa98c5bc18e1
Agustin Soria <soriaagustin8@gmail.com>
2023-08-18 07:51:55 -0300

    Adding logic and styles

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,22 +1,58 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './Quotes.css';
 
-function Quotes(category) {
-  const [data, setData] = useState([])
+function Quotes({ category }) {
+  const [data, setData] = useState([]);
+  const [loadState, setLoad] = useState(true);
+  const [error, setError] = useState(null);
 
+  const apiKey = 'PR9OrpSi7kbt4/xm76yLrQ==GDPbm1EnnixMX3cm';
   useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('https://jsonplaceholder.typicode.com/todos');
-      const json = await res.json();
-      setData(json);
-    };
-    fetchData();
-  }, [setData]);
+    axios.get('https://api.api-ninjas.com/v1/quotes?category=knowledge', {
+      headers: { 'X-Api-Key': apiKey },
+    })
+    .then((response) => {
+      setData(response.data);
+      setLoad(false);
+    })
+    .catch((error) => {
+      setError(error.message);
+      setLoad(false);
+    });
+  }, []);
+
+  if (loadState === true) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return (
+      <div>
+        Error: {error}
+      </div>
+    );
+  }
 
   return (
-    <ul>
-      {data.map((item) => (
-        <li key={item.id}>{item.title}</li>
-      ))}
-    </ul>
-  )
+    <div className="quote-display">
+      <h2>
+        Quotes about {category}
+      </h2>
+      <ul>
+        {data.map((quote, index) => (
+          <li key={index}>
+            <p>
+              {quote.quote}
+            </p>
+            <p>
+              {quote.author}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+export default Quotes;
