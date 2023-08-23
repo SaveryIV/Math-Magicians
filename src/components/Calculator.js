b8aa5b18508300a0ea311588cc465592f2a91a34
Agustin Soria <soriaagustin8@gmail.com>
2023-08-23 07:26:14 -0300

    Adding styles to the calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -16,40 +16,43 @@ function Calculator() {
   }
 
   return (
-    <div className="calculator">
-      <div className="display">
-        { numbers.total }
-        { numbers.operation }
-        { numbers.next }
-      </div>
-      <div className="buttons-row">
-        <CalculatorButton label="AC" onClick={() => handleClick('AC')} />
-        <CalculatorButton label="+/-" onClick={() => handleClick('+/-')} />
-        <CalculatorButton label="%" onClick={() => handleClick('%')} />
-        <CalculatorButton label="÷" onClick={() => handleClick('÷')} />
-      </div>
-      <div className="buttons-row">
-        <CalculatorButton label="7" onClick={() => handleClick('7')} />
-        <CalculatorButton label="8" onClick={() => handleClick('8')} />
-        <CalculatorButton label="9" onClick={() => handleClick('9')} />
-        <CalculatorButton label="x" onClick={() => handleClick('x')} />
-      </div>
-      <div className="buttons-row">
-        <CalculatorButton label="4" onClick={() => handleClick('4')} />
-        <CalculatorButton label="5" onClick={() => handleClick('5')} />
-        <CalculatorButton label="6" onClick={() => handleClick('6')} />
-        <CalculatorButton label="-" onClick={() => handleClick('-')} />
-      </div>
-      <div className="buttons-row">
-        <CalculatorButton label="1" onClick={() => handleClick('1')} />
-        <CalculatorButton label="2" onClick={() => handleClick('2')} />
-        <CalculatorButton label="3" onClick={() => handleClick('3')} />
-        <CalculatorButton label="+" onClick={() => handleClick('+')} />
-      </div>
-      <div className="buttons-row last-buttons">
-        <CalculatorButton label="0" onClick={() => handleClick('0')} />
-        <CalculatorButton label="." onClick={() => handleClick('.')} />
-        <CalculatorButton label="=" onClick={() => handleClick('=')} />
+    <div className="calculator-section">
+      <h1>Let`s do some math!</h1>
+      <div className="calculator">
+        <div className="display">
+          { numbers.total }
+          { numbers.operation }
+          { numbers.next }
+        </div>
+        <div className="buttons-row">
+          <CalculatorButton label="AC" onClick={() => handleClick('AC')} />
+          <CalculatorButton label="+/-" onClick={() => handleClick('+/-')} />
+          <CalculatorButton label="%" onClick={() => handleClick('%')} />
+          <CalculatorButton label="÷" onClick={() => handleClick('÷')} />
+        </div>
+        <div className="buttons-row">
+          <CalculatorButton label="7" onClick={() => handleClick('7')} />
+          <CalculatorButton label="8" onClick={() => handleClick('8')} />
+          <CalculatorButton label="9" onClick={() => handleClick('9')} />
+          <CalculatorButton label="x" onClick={() => handleClick('x')} />
+        </div>
+        <div className="buttons-row">
+          <CalculatorButton label="4" onClick={() => handleClick('4')} />
+          <CalculatorButton label="5" onClick={() => handleClick('5')} />
+          <CalculatorButton label="6" onClick={() => handleClick('6')} />
+          <CalculatorButton label="-" onClick={() => handleClick('-')} />
+        </div>
+        <div className="buttons-row">
+          <CalculatorButton label="1" onClick={() => handleClick('1')} />
+          <CalculatorButton label="2" onClick={() => handleClick('2')} />
+          <CalculatorButton label="3" onClick={() => handleClick('3')} />
+          <CalculatorButton label="+" onClick={() => handleClick('+')} />
+        </div>
+        <div className="buttons-row last-buttons">
+          <CalculatorButton label="0" onClick={() => handleClick('0')} />
+          <CalculatorButton label="." onClick={() => handleClick('.')} />
+          <CalculatorButton label="=" onClick={() => handleClick('=')} />
+        </div>
       </div>
     </div>
   );
